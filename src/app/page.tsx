fe24d35ab07ed97aa54462567a35d973b50effec
dhruvpatel2636 <dhruvbusiness2636@gmail.com>
2025-09-05 16:32:28 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <div className="mx-auto max-w-[820px] space-y-8">
+        <header className="">
+          <h1 className="font-inter mb-4 text-4xl text-[#0A0A0A]">
+            The quick brown fox jumps over the lazy dog
+          </h1>
+        </header>
+      </div>
+    </div>
+  )
+}
